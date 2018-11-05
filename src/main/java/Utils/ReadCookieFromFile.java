1b09234ab7cf296b91de2f12c52e9e5465d4a658
wangxinyu <wangxinyu@unionstech.cn>
2018-11-05 19:08:17 +0800

    read and write cookie for speed up login

---
+++ src/main/java/Utils/ReadCookieFromFile.java
@@ -0,0 +1,41 @@
+package Utils;
+
+
+import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ * @author WXY
+ * @version 创建时间：2018/11/1
+ */
+public class ReadCookieFromFile {
+    private final static Logger logger = Logger.getLogger(ReadCookieFromFile.class);
+
+    public static String[] getTimeFromTxt(String filePath) {
+        String[] resultMap = new String[2];
+        try {
+            File file = new File(filePath);
+            BufferedReader reader = null;
+            if (file.isFile() && file.exists()) {
+                reader = new BufferedReader(new FileReader(file));
+                String tempString = "";
+                while ((tempString = reader.readLine()) != null) {
+                    String[] tempString2 = tempString.split(",");
+                    resultMap[0] = tempString2[0];
+                    resultMap[1] = tempString2[1];
+                }
+                if (reader != null)
+                    reader.close();
+            } else {
+                logger.error("找不到指定的文件");
+            }
+        } catch (Exception e) {
+            logger.error("读取文件内容出错。 " + e.getMessage());
+        }
+        return resultMap;
+    }
+
+}
---
+++ src/main/java/Utils/WriteCookieToFile.java
@@ -0,0 +1,31 @@
+package Utils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+/**
+ * @author WXY
+ * @version 创建时间：2018/11/1
+ */
+public class WriteCookieToFile {
+
+    public static void writeCookieToFile(String content, long time) {
+        BufferedWriter bw = null;
+        try {
+            File file = new File("cookie.txt");
+//            if (!file.exists()) {
+            file.createNewFile();
+//            }
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            bw = new BufferedWriter(fw);
+            bw.write(String.valueOf(time) + ",");
+            bw.write(content);
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
