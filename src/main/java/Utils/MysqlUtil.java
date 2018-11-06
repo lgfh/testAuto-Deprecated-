6d5afd42fff13d6f257147319534a950fbe19b42
wangxinyu <wangxinyu@unionstech.cn>
2018-11-06 17:12:27 +0800

    create a test_db database in mysql to test and accept return value

--- src/main/java/Utils/MysqlUtil.java
+++ src/main/java/Utils/MysqlUtil.java
@@ -8,6 +8,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author WXY
@@ -16,31 +17,49 @@ import java.sql.SQLException;
 public class MysqlUtil {
     private final static Logger logger = Logger.getLogger(MysqlUtil.class);
 
-    public static Connection getConnectionAndTest(String ip, String passwd) {
+    public static Connection getConnectionAndTest(String ip, String passwd) throws ClassNotFoundException, SQLException {
         String driver = "com.mysql.cj.jdbc.Driver";  //获取mysql数据库的驱动类
         String url = String.format("jdbc:mysql://%s:3306/", ip); //连接数据库
         String name = "root";//连接mysql的用户名
         String pwd = passwd;//连接mysql的密码
 
-        try {
-            Class.forName(driver);
-            Connection conn = DriverManager.getConnection(url, name, pwd);//获取连接对象
-            if (!conn.isClosed()) {
-                logger.info("Test connection success");
-                return conn;
-            } else {
-                logger.info("Test connection failed");
-                return null;
-            }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        } catch (SQLException e) {
-            e.printStackTrace();
+        Class.forName(driver);
+        Connection conn = DriverManager.getConnection(url, name, pwd);//获取连接对象
+        if (!conn.isClosed()) {
+            logger.info("Test connection success");
+            return conn;
+        } else {
+            logger.info("Test connection failed");
             return null;
         }
 
+
     }
+
+    public static int getConnectionAndQueryResult(String ip, String passwd, String sql) throws ClassNotFoundException, SQLException {
+        String driver = "com.mysql.cj.jdbc.Driver";  //获取mysql数据库的驱动类
+        String url = String.format("jdbc:mysql://%s:3306/", ip); //连接数据库
+        String name = "root";//连接mysql的用户名
+        String pwd = passwd;//连接mysql的密码
+
+        Class.forName(driver);
+        Connection conn = DriverManager.getConnection(url, name, pwd);//获取连接对象
+        Statement smt = conn.createStatement();
+        int i = smt.executeUpdate(sql);
+
+        if (!conn.isClosed()) {
+            if (i == 1) {
+                logger.info("Test connection success");
+            }
+            return i;
+        } else {
+            logger.info("Test connection failed");
+            return 0;
+        }
+
+
+    }
+
 
     public static void closeConnection(Connection conn) {
         try {
