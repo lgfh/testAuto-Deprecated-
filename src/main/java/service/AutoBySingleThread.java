49d4532eb3330f69edcf7a2c0dfe77d7d80119e1
wangxinyu <wangxinyu@unionstech.cn>
2018-11-06 17:19:52 +0800

    single thread to delete several DBs, change the max value of i before every use

---
+++ src/main/java/service/AutoBySingleThread.java
@@ -0,0 +1,31 @@
+package service;
+
+import org.apache.log4j.Logger;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author WXY
+ * @version 创建时间：2018/11/5
+ */
+public class AutoBySingleThread {
+    private final static Logger logger = Logger.getLogger(AutoByMutiThread.class);
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        logger.info("创建单线程池");
+        int i = 1;
+        while (i <= 2) {
+            RunnableThreadDelDB t = new RunnableThreadDelDB();
+            executorService.execute(t);
+            i++;
+        }
+
+        executorService.shutdown();
+
+    }
+}
+
+
+
