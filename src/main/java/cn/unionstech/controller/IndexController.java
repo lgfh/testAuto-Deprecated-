b178ec80bc5afe0bced2bd8057a395f72cd5810f
wangxinyu <wangxinyu@unionstech.cn>
2018-11-20 11:18:21 +0800

    add request mapping controller

--- src/main/java/cn/unionstech/controller/IndexController.java
+++ src/main/java/cn/unionstech/controller/IndexController.java
@@ -1,10 +1,14 @@
 package cn.unionstech.controller;
 
 import cn.unionstech.application.AutoCreateGPUVm;
+import cn.unionstech.application.AutoCreateMysqlAndTestConnection;
+import cn.unionstech.application.AutoCreateRedisAndTest;
 import cn.unionstech.application.AutoCreateVm;
 import cn.unionstech.application.AutoDestroyDB;
 import cn.unionstech.application.AutoDestroyVm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,29 +19,65 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    @Autowired
+    AutoCreateVm autoCreateVm;
+
+    @Autowired
+    AutoCreateGPUVm autoCreateGPUVm;
+
+    @Autowired
+    AutoCreateMysqlAndTestConnection autoCreateMysqlAndTestConnection;
+
+    @Autowired
+    AutoDestroyDB autoDestroyDB;
+
+    @Autowired
+    AutoDestroyVm autoDestroyVm;
+
+    @Autowired
+    AutoCreateRedisAndTest autoCreateRedisAndTest;
+
     @RequestMapping(path = "/CreateVm")
 //    @ResponseBody
+    public String getVmCreateResult(@RequestParam(value = "zone", defaultValue = "华东一区") String zone) {
+
+        return autoCreateVm.autoCreateVM(zone);
+//        return "Hello";
+    }
+
+    @RequestMapping(path = "/CreateTestVm")
     public String getVmCreateResult() {
 
-        return AutoCreateVm.autoCreateVM();
-//        return "Hello";
+        return autoCreateVm.autoCreateVM("华中二区");
     }
 
     @RequestMapping(path = "/CreateGpuVm")
     @ResponseBody
     public String getGpuVmCreateResult() {
 
-        return AutoCreateGPUVm.autoCreateGpuVM();
+        return autoCreateGPUVm.autoCreateGpuVM();
     }
 
 
     @RequestMapping("/DelVm")
     public String getVmDelResult() {
-        return AutoDestroyVm.autoDestroyVM();
+        return autoDestroyVm.autoDestroyVM();
     }
 
     @RequestMapping("/DelDB")
-    public String getDbDelResult() {
-        return AutoDestroyDB.autoDestroyDB();
+    public String getDbDelResult(@RequestParam(value = "zone", defaultValue = "华东一区") String zone) {
+        return autoDestroyDB.autoDestroyDB(zone);
     }
+
+    @RequestMapping("/CreateMysqlTest")
+    public String getMysqlTestResult(@RequestParam(value = "zone", defaultValue = "华东一区") String zone) {
+        return autoCreateMysqlAndTestConnection.autoCreateMysqlAndTestConnection(zone);
+    }
+
+    @RequestMapping("/CreateRedisTest")
+    public String getRedisTestResult(@RequestParam(value = "zone", defaultValue = "华东一区") String zone) {
+        return autoCreateRedisAndTest.autoCreateRedisAndTest(zone);
+    }
+
+
 }
